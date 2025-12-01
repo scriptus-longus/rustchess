ac5ff67c31cad31ec0ff50022914c56e19251f55
scriptus longus <scriptus-longus@protonmail.com>
2025-12-01 03:06:21 +0100

    some small cleanup

--- src/movegen.rs
+++ src/movegen.rs
@@ -1,13 +1,8 @@
 use crate::board::{Board, Player, Pieces};
-use crate::board::BitBoard;
 
 pub static KNIGHT_MOVES_LOOKUP: [u64; 64] = [
 132096, 329728, 659712, 1319424, 2638848, 5277696, 10489856, 4202496, 33816580, 84410376, 168886289, 337772578, 675545156, 1351090312, 2685403152, 1075839008, 8657044482, 21609056261, 43234889994, 86469779988, 172939559976, 345879119952, 687463207072, 275414786112, 2216203387392, 5531918402816, 11068131838464, 22136263676928, 44272527353856, 88545054707712, 175990581010432, 70506185244672, 567348067172352, 1416171111120896, 2833441750646784, 5666883501293568, 11333767002587136, 22667534005174272, 45053588738670592, 18049583422636032, 145241105196122112, 362539804446949376, 725361088165576704, 1450722176331153408, 2901444352662306816, 5802888705324613632, 11533718717099671552, 4620693356194824192, 288234782788157440, 576469569871282176, 1224997833292120064, 2449995666584240128, 4899991333168480256, 9799982666336960512, 1152939783987658752, 2305878468463689728, 1128098930098176, 2257297371824128, 4796069720358912, 9592139440717824, 19184278881435648, 38368557762871296, 4679521487814656, 9077567998918656
 ];
-
-const K1: u64 = 0x5555555555555555; // 0101...
-const K2: u64 = 0x3333333333333333; // 00110011...
-const K4: u64 = 0x0f0f0f0f0f0f0f0f; //  4-bit swaps
 
 #[derive(Debug, PartialEq)]
 pub struct Move {
@@ -187,12 +182,6 @@ impl MoveGen {
       // check for piece collision with player pieces
       targets = targets & !board.get_player_mask(player);
 
-      /*while targets != 0 {
-        let to_sq = targets.trailing_zeros();
-        targets ^= 1u64 << to_sq;
-
-        moves.push(Move{from: from_sq, to: to_sq});
-      }*/
       moves.extend(MoveGen::collect_moves(from_sq, targets));
     }
 
@@ -224,12 +213,6 @@ impl MoveGen {
       let targets = ray_u | ray_d | ray_l | ray_r; 
 
       moves.extend(MoveGen::collect_moves(from_sq, targets));
-      /*while targets != 0 {
-        let to_sq = targets.trailing_zeros();
-        targets ^= 1u64 << to_sq;
-
-        moves.push(Move{from: from_sq, to: to_sq});
-      }*/
 
       rook_mask ^= 1u64 << from_sq;
     }
@@ -287,6 +270,7 @@ impl MoveGen {
     moves.extend(MoveGen::pawn_moves(board, player));
     moves.extend(MoveGen::knight_moves(board, player));
     moves.extend(MoveGen::rook_moves(board, player));
+    moves.extend(MoveGen::bishop_moves(board, player));
   
     moves
   }
