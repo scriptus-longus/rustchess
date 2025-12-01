7a19c2a30e1e352bd771c9628161897faec3362b
scriptus longus <scriptus-longus@protonmail.com>
2025-12-01 02:21:00 +0100

    added rook movement

--- src/movegen.rs
+++ src/movegen.rs
@@ -1,14 +1,25 @@
 use crate::board::{Board, Player, Pieces};
-//use crate::board::BitBoard;
+use crate::board::BitBoard;
 
 pub static KNIGHT_MOVES_LOOKUP: [u64; 64] = [
 132096, 329728, 659712, 1319424, 2638848, 5277696, 10489856, 4202496, 33816580, 84410376, 168886289, 337772578, 675545156, 1351090312, 2685403152, 1075839008, 8657044482, 21609056261, 43234889994, 86469779988, 172939559976, 345879119952, 687463207072, 275414786112, 2216203387392, 5531918402816, 11068131838464, 22136263676928, 44272527353856, 88545054707712, 175990581010432, 70506185244672, 567348067172352, 1416171111120896, 2833441750646784, 5666883501293568, 11333767002587136, 22667534005174272, 45053588738670592, 18049583422636032, 145241105196122112, 362539804446949376, 725361088165576704, 1450722176331153408, 2901444352662306816, 5802888705324613632, 11533718717099671552, 4620693356194824192, 288234782788157440, 576469569871282176, 1224997833292120064, 2449995666584240128, 4899991333168480256, 9799982666336960512, 1152939783987658752, 2305878468463689728, 1128098930098176, 2257297371824128, 4796069720358912, 9592139440717824, 19184278881435648, 38368557762871296, 4679521487814656, 9077567998918656
 ];
 
+const K1: u64 = 0x5555555555555555; // 0101...
+const K2: u64 = 0x3333333333333333; // 00110011...
+const K4: u64 = 0x0f0f0f0f0f0f0f0f; //  4-bit swaps
+
 #[derive(Debug, PartialEq)]
 pub struct Move {
   from: u32,
   to: u32,
+}
+
+pub enum Dir {
+  U,
+  D,
+  R,
+  L
 }
 
 impl Move {
@@ -26,6 +37,36 @@ impl Move {
 pub struct MoveGen;
 
 impl MoveGen {
+  fn ray_mask(from: u32, blocker_mask: u64, capture_mask: u64, direction: Dir) -> u64 {
+    let mut ret = 0;
+    let st_size: i32 = match direction {
+      Dir::U => 8,
+      Dir::D => -8,
+      Dir::L => -1,
+      Dir::R => 1,
+    };
+
+    //let mut p_bb = 1u64 << from;
+    let start_rank: i32 = (from as i32) / 8;
+    let start_file: i32 = (from as i32) % 8;
+
+    let mut sq = (from as i32) + st_size;
+    while sq < 64 && sq >= 0 && (sq / 8 == start_rank || sq % 8 == start_file)  {
+      let to_mask = 1u64 << sq;
+
+      if to_mask & blocker_mask != 0 {break;}
+      if to_mask & capture_mask != 0 {
+        ret |= to_mask;
+        break;
+      }
+
+      ret |= to_mask;
+      sq += st_size;
+    }
+
+    ret
+  }
+
   fn pawn_moves(board: &Board, player: Player) -> Vec<Move> {
     let mut moves: Vec<Move> = vec![];
 
@@ -107,14 +148,63 @@ impl MoveGen {
       // check for piece collision with player pieces
       targets = targets & !board.get_player_mask(player);
 
-      //let test = BitBoard{bitboard: board.get_player_mask(player)};
-
-      while targets != 0 {
+      /*while targets != 0 {
         let to_sq = targets.trailing_zeros();
         targets ^= 1u64 << to_sq;
 
         moves.push(Move{from: from_sq, to: to_sq});
-      }
+      }*/
+      moves.extend(MoveGen::collect_moves(from_sq, targets));
+    }
+
+    moves
+  }
+
+
+
+  pub fn rook_moves(board: &Board, player: Player) -> Vec<Move> {
+    let mut moves = vec![];
+
+    let opp =  match player {
+      Player::White => Player::Black,
+      Player::Black => Player::White,
+    };
+
+    let block_mask = board.get_player_mask(player);
+    let capture_mask = board.get_player_mask(opp);
+
+    let mut rook_mask = board.get_pieceboard(player, Pieces::Rook).bitboard;
+    while rook_mask != 0 {
+      let from_sq = rook_mask.trailing_zeros();
+
+      let ray_u = MoveGen::ray_mask(from_sq, block_mask, capture_mask, Dir::U);
+      let ray_d = MoveGen::ray_mask(from_sq, block_mask, capture_mask, Dir::D);
+      let ray_l = MoveGen::ray_mask(from_sq, block_mask, capture_mask, Dir::L);
+      let ray_r = MoveGen::ray_mask(from_sq, block_mask, capture_mask, Dir::R);
+
+      let targets = ray_u | ray_d | ray_l | ray_r; 
+
+      moves.extend(MoveGen::collect_moves(from_sq, targets));
+      /*while targets != 0 {
+        let to_sq = targets.trailing_zeros();
+        targets ^= 1u64 << to_sq;
+
+        moves.push(Move{from: from_sq, to: to_sq});
+      }*/
+
+      rook_mask ^= 1u64 << from_sq;
+    }
+  
+    moves 
+  }
+
+  fn collect_moves(from: u32, mut targets: u64) -> Vec<Move>{
+    let mut moves = vec![];
+    while targets != 0 {
+      let to_sq = targets.trailing_zeros();
+      targets ^= 1u64 << to_sq;
+
+      moves.push(Move{from: from, to: to_sq});
     }
 
     moves
@@ -125,7 +215,8 @@ impl MoveGen {
 
     moves.extend(MoveGen::pawn_moves(board, player));
     moves.extend(MoveGen::knight_moves(board, player));
-
+    moves.extend(MoveGen::rook_moves(board, player));
+  
     moves
   }
 }
@@ -155,6 +246,20 @@ mod tests {
         x.print_board();
         let moves = MoveGen::knight_moves(&x, Player::White);
         assert_eq!(moves, vec![Move{from: 4*8 + 1, to: 48}, Move{from: 4*8 + 1, to: 50}])
+      },
+      Err(_) => {println!("could not create board"); assert_eq!(1,0)},
+      
+    }
+  }
+
+  #[test]
+  fn test_rook_moves() {
+    let board = Board::from_fen("RP6/8/p7/8/8/8/8/8 w HAha - 0 1");
+    match board {
+      Ok(x) => {
+        x.print_board();
+        let moves = MoveGen::rook_moves(&x, Player::White);
+        assert_eq!(moves, vec![Move{from: 7*8, to: 5*8}, Move{from: 7*8, to: 6*8}])
       },
       Err(_) => {println!("could not create board"); assert_eq!(1,0)},
       
