007d3b2e913bc143e11a95e22bf58583b318e632
scriptus longus <scriptus-longus@protonmail.com>
2025-12-17 03:05:46 +0100

    perft tests work for now

--- tests/perft.rs
+++ tests/perft.rs
@@ -34,4 +34,24 @@ mod perft_test {
       assert_eq!(perft_res, res, "FEN1: Failed at depth {}: expected {} but got {}", depth, res, perft_res);
     }
   }
+
+  #[test]
+  pub fn perft_test_fen_4() {
+    for (depth, res) in [1, 6, 264, 9467].into_iter().enumerate() {
+      let mut game = Game::from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1").unwrap();
+      let perft_res = perft::perft(&mut game, depth);
+
+      assert_eq!(perft_res, res, "FEN1: Failed at depth {}: expected {} but got {}", depth, res, perft_res);
+    }
+  }
+
+  #[test]
+  pub fn perft_test_fen_5() {
+    for (depth, res) in [1, 44, 1486, 62_379].into_iter().enumerate() {
+      let mut game = Game::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8").unwrap();
+      let perft_res = perft::perft(&mut game, depth);
+
+      assert_eq!(perft_res, res, "FEN1: Failed at depth {}: expected {} but got {}", depth, res, perft_res);
+    }
+  }
 }
